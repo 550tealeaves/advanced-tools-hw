d4c52115b6457cc69bae5c463d63b88a23f51e03
550tealeaves <nastasia.claire89@gmail.com>
2023-04-16 17:39:25 -0400

    Finished JS tutorial for beginners
    https://www.youtube.com/watch?v=W6NZfCO5SIk&t=8s

--- js-basics/script.js
+++ js-basics/script.js
@@ -9,22 +9,22 @@
 let firstName = 'Mosh';
 let lastName = 'Hamedani';
 
-// let interestRate = 0.3;
-// interestRate = 1;
-// console.log(interestRate); //let allows you to reassign variable
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate); //let allows you to reassign variable
 
-// const interestRate2 = 0.3;
+const interestRate2 = 0.3;
 // interestRate2 = 1;
-// console.log(interestRate2); //error b/c cannot reassign a constant
+console.log(interestRate2); //error b/c cannot reassign a constant
 
 
 //2 DATA TYPES: PRIMITIVE(VALUE TYPES) & REFERENCE TYPES
 //PRIMITIVE
-// let nameAgain = 'Moshi'; //string literal
-// let age = 30; //number literal
-// let isApproved = true; //boolean literal
-// let firstNameAgain = undefined; //undefined (not usually used) - type of var = undefined
-// let lastNameAgain = null; //null - used to clear value of variable - type of var = object
+let occupation = 'technician'; //string literal
+let weight = 130; //number literal
+let isApproved = true; //boolean literal
+let lifeGoal = undefined; //undefined (not usually used) - type of var = undefined
+let travelPlans = null; //null - used to clear value of variable - type of var = object
 
 //OBJECTS - contain multiple related variables w/ diff properties
 //declare person object 
@@ -44,3 +44,60 @@ person['name'] = 'Mary'; //replaces John w/ Mary
 console.log(person);
 
 console.log(person.name); 
+
+//ARRAYS - list of objects - ex: list of colors, list of groceries
+// [] = array literal 
+//each element in the array has an index, which determines position in array
+let selectedColors = [
+    'red',
+    'blue'
+];
+selectedColors[2] = 'green'; //this will add a 3rd element to the array (position 2)
+selectedColors[3] = 1; //adds 4th element to array (position 3) - can mix data types
+
+console.log(selectedColors);
+console.log(selectedColors[0]); //will show 1st element in array
+
+//Array is an object = has property-value pairs - can use dot notation to see properties
+console.log(selectedColors.length);
+
+
+//FUNCTIONS - statements that perform task/calculation
+//Parameter - variable (input) given at time of function declaration
+//Argument - value supplied to the parameter
+
+
+//Performs a task
+function greet(name, lastName) { //name is a PARAMETER (input) - only meaningful INSIDE function
+    console.log('Hello ' + name + ' ' + lastName); //add ; here b/c using statement
+}
+
+//'John' = ARGUMENT - value supplied to the parameter (name) passed into function
+//'Smith' = ARGUMENT - value supplied to parameter (lastName) passed into function
+greet('John', 'Smith'); 
+
+//Mary = ARGUMENT for name parameter
+//Thompson = ARGUMENT for lastName parameter
+greet('Mary', 'Thompson');  
+
+
+//functions w/ multiple parameters - can add more inputs
+//will need to add arguments that correspond to the addtional parameter(s) or else will return undefined
+
+
+//TYPES OF FUNCTIONS
+
+//Calcuating task function
+function square(number){
+    return number * number;
+}
+
+//2 = ARGUMENT passed into the number parameter for function square
+//You can then set that value of square(2) to another variable 
+let squaredNumber = square(2);
+console.log(squaredNumber);
+
+// you also can declare the variable within the console log instead of calling it separately 
+
+console.log(square(2)); //it calls function square, passing in argument of 2, and then displaying result in the console 
+//there are 2 function calls here - square(2); & console.log();
